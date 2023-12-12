17f8a2d1ce89b926325489257cfa1c5453776c76
elvinkyungu <elvinkyungu.75@gmail.com>
2023-12-12 14:43:29 +0200

    💚  redirecting user wehn is'nt login

--- src/views/user.vue
+++ src/views/user.vue
@@ -1,14 +1,24 @@
 <template>
   <h1>Bienvenue, {{ userStore.name }}!</h1>
+  <button @click="logoutUser">Déconnexion</button>
 </template>
 
 <script lang="ts" setup>
 import { onMounted } from 'vue';
 import { useUserStore } from '@/stores/store';
+import { useRouter } from 'vue-router'
 
 const userStore = useUserStore();
+const router = useRouter();
 
+const logoutUser = () => {
+  userStore.clearUserData();
+  router.push('/'); 
+}
 onMounted(() => {
+  if (!userStore.token) {
+    router.push('/'); // Remplacez '/login' par le chemin de votre page de connexion
+  }
   userStore.restoreUserData();
 });
 </script>
