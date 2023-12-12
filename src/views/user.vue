06729c37ec685da6b20d53bc78ea5a6ccd0688e8
elvinkyungu <elvinkyungu.75@gmail.com>
2023-12-12 13:57:25 +0200

    👹👺 fix : resolve refresh datas with localstorage

--- src/views/user.vue
+++ src/views/user.vue
@@ -1,8 +1,14 @@
 <template>
   <h1>Bienvenue, {{ userStore.name }}!</h1>
 </template>
-<script lang="ts" setup>
-  import { useUserStore } from '@/stores/store';
 
-  const userStore = useUserStore();
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+import { useUserStore } from '@/stores/store';
+
+const userStore = useUserStore();
+
+onMounted(() => {
+  userStore.restoreUserData();
+});
 </script>
