9bfc55c1bec63ec09e35a21409a25c8c57c4090e
elvinkyungu <elvinkyungu.75@gmail.com>
2023-12-12 12:09:02 +0200

    💚 same logic to loading spinner when btn component clicked

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,60 +1,8 @@
-
-<script setup lang="ts">
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import PocketBase from 'pocketbase';
-import { ref } from 'vue';
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-
-interface User {
-  mail: string | null;
-  password: string | null;
-}
-
-const isLogin = ref(false);
-const errorMessage = ref('');
-
-const user = ref<User>({
-  mail: null,
-  password: null,
-});
-
-const pb = new PocketBase('https://bat-her.pockethost.io');
-const message = ref('Connectez-vous d\'un simple geste');
-
-const loginUser = async () => {
-  try {
-    isLogin.value = true; // Activer la barre de progression
-    const result = await pb.collection('users').authWithPassword(
-      user.value.mail || '',
-      user.value.password || '',
-    );
-    console.log(result);
-    message.value = 'Connecté avec succès!';
-    router.push('/user')
-  } catch (error) {
-    console.error('Erreur lors de la connexion', error);
-    errorMessage.value = 'Erreur de connexion. Veuillez réessayer.'; 
-  } finally {// Désactiver la barre de progression après l'authentification ou en cas d'erreur
-  }
-};
-</script>
-
 <template>
   <div>
     <div 
-      v-if="!isLogin"
-      class="h-screen relative flex items-center justify-center">
+      class="h-screen relative flex items-center justify-center"
+    >
       <Card class="w-[35rem] border-2">
         <CardHeader class="space-y-1">
           <CardTitle class="text-2xl">
@@ -112,18 +60,70 @@ const loginUser = async () => {
           </div>
         </CardContent>
         <CardFooter>
-          <Button class="w-full" @click="loginUser">
-            Connectez-vous
+          <Button 
+            class="w-full p-7 text-lg" 
+            @click="loginUser"
+            :class="{ 'pointer-events-none': isLogin, 'cursor-not-allowed': isLogin }"
+          >
+            <Spinner v-if="isLogin"/>
+            <span v-else>Connectez-vous</span>
           </Button>
         </CardFooter>
       </Card>
     </div>
-    <div role="status" v-if="isLogin">
-      <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-      </svg>
-      <span class="sr-only">Loading...</span>
-    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import Spinner from '@/components/Spinner.vue';
+import PocketBase from 'pocketbase';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+interface User {
+  mail: string | null;
+  password: string | null;
+}
+
+const isLogin = ref(false);
+const errorMessage = ref('');
+
+const user = ref<User>({
+  mail: null,
+  password: null,
+});
+
+const pb = new PocketBase('https://bat-her.pockethost.io');
+const message = ref('Connectez-vous d\'un simple geste');
+
+const loginUser = async () => {
+  try {
+    isLogin.value = true; 
+    const result = await pb.collection('users').authWithPassword(
+      user.value.mail || '',
+      user.value.password || '',
+    );
+    console.log(result);
+    message.value = 'Connecté avec succès!';
+    router.push('/user')
+  } catch (error) {
+    isLogin.value = false; 
+    console.error('Erreur lors de la connexion', error);
+    errorMessage.value = 'Erreur de connexion. Veuillez réessayer.'; 
+  } finally {
+    isLogin.value = false; 
+  }
+};
+</script>
