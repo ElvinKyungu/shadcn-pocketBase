0d8550d90adb6eb9d39e646adfc53ef7ed53c98a
elvinkyungu <elvinkyungu.75@gmail.com>
2024-01-08 08:41:55 +0200

    feat: install GSAP library

---
+++ src/components/dashboard/lateral/LateralNotifications.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2 class="text-2xl font-bold mb-4">Notifications récentes</h2>
+
+    <div class="space-y-4">
+      <div class="p-4 bg-white border rounded-xl text-gray-800 space-y-2">
+        <div class="flex justify-between">
+          <div class="text-gray-400 text-xs">Important</div>
+          <div class="text-gray-400 text-xs">Il y a 4h</div>
+        </div>
+        <a href="javascript:void(0)" class="font-bold hover:text-yellow-800 hover:underline">Blog and social posts</a>
+        <div class="text-sm text-gray-600">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="text-gray-800 inline align-middle mr-1" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+          </svg>La date limite c'est aujourd'hui
+        </div>
+      </div>
+    </div>
+    <div class="mt-20">
+      <h2 class="text-2xl font-bold mb-4">Dernière tâche ajoutée</h2>
+
+      <div class="space-y-4">
+        <div class="p-4 bg-white border rounded-xl text-gray-800 space-y-2">
+          <div class="flex justify-between">
+            <div class="text-gray-400 text-xs">Homework</div>
+            <div class="text-gray-400 text-xs">Il y a 4h</div>
+          </div>
+          <a href="javascript:void(0)" class="font-bold hover:text-yellow-800 hover:underline">Blog and social posts</a>
+          <div class="text-sm text-gray-600">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="text-gray-800 inline align-middle mr-1" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+            </svg>La date limite c'est aujourd'hui
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/dashboard/lateral/LateralStats.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2 class="text-2xl font-bold mb-4">Stats</h2>
+
+    <div class="grid grid-cols-2 gap-4">
+      <div class="col-span-2">
+        <div class="p-4 bg-green-100 rounded-xl">
+          <div class="font-bold text-xl text-gray-800 leading-none">Hello, <br>Elvin</div>
+          <div class="mt-5">
+            <button type="button" class="inline-flex items-center justify-center py-2 px-3 rounded-xl bg-white text-gray-800 hover:text-green-500 text-sm font-semibold transition">
+              Start working
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 bg-yellow-100 rounded-xl text-gray-800">
+        <div class="font-bold text-2xl leading-none">20</div>
+        <div class="mt-2">Tasks finished</div>
+      </div>
+      <div class="p-4 bg-yellow-100 rounded-xl text-gray-800">
+        <div class="font-bold text-2xl leading-none">4</div>
+        <div class="mt-2">Remaining tasks</div>
+      </div>
+      <div class="col-span-2">
+        <div class="p-4 bg-purple-100 rounded-xl text-gray-800">
+          <div class="font-bold text-xl leading-none">Your daily plan</div>
+          <div class="mt-2">20 of 24 completed</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
