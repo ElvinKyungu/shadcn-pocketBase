12f8aefe67d1fe335939e1d0efe990559623081e
elvinkyungu <elvinkyungu.75@gmail.com>
2024-01-25 11:36:11 +0200

    feat: uninstall Password rendom library
    👨‍💻 Author: Elvin Kyungu

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,6 @@ import { pb } from "@/pocketbase/pocket";
 import { useUserStore } from "@/stores/store";
 import { createUSer, creatUserWithGoogle } from "@/types/user";
 import { useRouter } from "vue-router";
-import { generate } from 'generate-password';
 
 const userStore = useUserStore();
 const router = useRouter()
@@ -23,10 +22,10 @@ const signupUser = async (newUser: createUSer) => {
 const loginUserWithGoogle = async (newUser: creatUserWithGoogle) => {
   try {
 
-    //const result = await pb.collection('users').create(newUser);
-    //console.log(result);
-    //userStore.setUserData({ userID: result.id, name: result.name, token: result.token });
-    //router.push('/user')
+    const result = await pb.collection('users').create(newUser);
+    console.log(result);
+    userStore.setUserData({ userID: result.id, name: result.name, token: result.token });
+    router.push('/user')
   } catch (error) {
     console.error('Erreur lors de la connexion', error);
   } finally {
@@ -38,13 +37,7 @@ const getUserDataWithGoogle = async () => {
     const authData = await pb.collection('users').authWithOAuth2({ provider: 'google' });
     console.log(authData.meta);
     if (authData && authData.meta) {
-      const generator = require('generate-password');
-      const passwordForGoogleUser = generator.generate({
-        length: 10,
-        numbers: true
-      });
-      // 'uEyMTw32v9'
-      console.log(passwordForGoogleUser);
+      const passwordForGoogleUser = '';
       await signupUser({
         username: authData.meta.username,
         name: authData.meta.name,
