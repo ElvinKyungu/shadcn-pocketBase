4eccee93812e502380781112bf3156871eb2a87d
elvinkyungu <elvinkyungu.75@gmail.com>
2024-01-26 13:25:19 +0200

    👺👹 fix: fixing errorMessage modal
    🚀 Author: Elvin Kyungu

--- src/components/dashboard/lateral/LateralAddTask.vue
+++ src/components/dashboard/lateral/LateralAddTask.vue
@@ -18,7 +18,13 @@
       <div class="relative p-4 w-full max-w-2xl max-h-full z-50">
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
           <div v-if="isSubmit" class="h-96 flex justify-center items-center">
-            <Spinner/>
+            <div class="p-4 md:p-5 text-center" v-if="errorMessage !==''">
+              <svg class="mx-auto mb-4 text-red-700 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+              </svg>
+              <span class="text-red-700 text-lg">{{ errorMessage}}</span>
+            </div>
+            <Spinner v-else/>
           </div>
           <div v-else>
             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
@@ -265,14 +271,22 @@ async function fetchUsers() {
 
 async function addNewTaskLoc (){
   if (validateFormTask(newTask.value)){
+    isSubmit.value = true;
     if(userStore.userID !== null){
-      const formattedDeadline: string = format(selectedDate.value, "yyyy-MM-dd HH:mm:ss");
-      const formattedupdatedAt: string = format(date, "yyyy-MM-dd HH:mm:ss");
+      try{
+        const formattedDeadline: string = format(selectedDate.value, "yyyy-MM-dd HH:mm:ss");
+        const formattedupdatedAt: string = format(date, "yyyy-MM-dd HH:mm:ss");
 
-      newTask.value.deadline = formattedDeadline
-      newTask.value.updatedAt = formattedupdatedAt
-      console.log(newTask.value.userID);
-      addNewTaskTEST(newTask.value)
+        newTask.value.deadline = formattedDeadline
+        newTask.value.updatedAt = formattedupdatedAt
+        await addNewTaskTEST(newTask.value)
+        setTimeout(()=>{
+          closeModal()
+        }, 2000)
+      }catch{
+        errorMessage.value = "Impossible d'ajouter une tâche"
+        isSubmit.value = false;
+      }
     }else{
       userStore.clearUserData()
     }
